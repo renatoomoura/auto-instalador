3e252482bcf97b539d21f79fd1ac6de4abb4aa58
Ricardo Alexandre <49127332+VespertineWeb@users.noreply.github.com>
2022-05-28 14:04:25 -0300

    Whaticket

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -10,7 +10,6 @@
 #   WHITE
 #   CYAN_LIGHT
 #   RED
-#   BLUE
 #   GREEN
 #   YELLOW
 # Arguments:
@@ -22,14 +21,27 @@ print_banner() {
 
   printf "\n\n"
 
-  printf "${BLUE}";
-  printf "                 ████████ ██     ██  ███████  ████    ██  █████████   █████████  ████████       ████████  ████████  ████    ████              \n";
-  printf "                 ██    ██ ██     ██  ██       ██ ██   ██        ███   ██     ██  ██    ██       ██        ██    ██  ██ █    █ ██   \n";
-  printf "                 ██    ██ ██  █  ██  ███████  ██  ██  ██     ███      █████████  ████████       ██        ██    ██  ██  █  █  ██    \n";
-  printf "                 ██    ██ ██ ███ ██  ██       ██   ██ ██  ███         ██     ██  ██             ██        ██    ██  ██   ██   ██ \n";
-  printf "                 ████████  ███ ███   ███████  ██    ████  █████████   ██     ██  ██        ██   ████████  ████████  ██        ██                \n";
+  printf "${GREEN}";
+  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
+  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
+  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
+  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
+  printf "                                                    █  ▄█▀        \n";
+  printf "                                                    ▀▀▀▀          \n";
+  printf "${NC}";
+
   printf "\n"
- 
+
+  printf "${GREEN}";
+  printf "██     ██ ██   ██  █████  ████████ ██  ██████ ██   ██ ███████ ████████\n";
+  printf "██     ██ ██   ██ ██   ██    ██    ██ ██      ██  ██  ██         ██   \n";
+  printf "██  █  ██ ███████ ███████    ██    ██ ██      █████   █████      ██   \n";
+  printf "██ ███ ██ ██   ██ ██   ██    ██    ██ ██      ██  ██  ██         ██   \n";
+  printf " ███ ███  ██   ██ ██   ██    ██    ██  ██████ ██   ██ ███████    ██   \n";
   printf "${NC}";
 
   printf "\n"
